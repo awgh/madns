9ec6034a5d0d4e4fc5a058d4e16e4ae92045cab4
sig1nt <sigint@scriptalert.one>
2025-01-05 19:09:47 -0800

    Add round robin dns rebinding

--- main.go
+++ main.go
@@ -12,6 +12,8 @@ import (
 	"strings"
 	"syscall"
 	"time"
+	"sync"
+	"sync/atomic"
 
 	"github.com/miekg/dns"
 )
@@ -37,7 +39,16 @@ type MadnsSubConfig struct {
 	NotifyEmail string
 	Respond     string
 	NotifySlack string
+	Rebind	    *MadnsRebindConfig
 }
+
+// MadnsRebindConfig - Rebind requires more data and I'd like to add strategies one day
+type MadnsRebindConfig struct {
+	Addrs []string
+}
+
+// Yes, big ugly global variable, but :shrug:
+var RebindMap sync.Map = sync.Map{}
 
 func main() {
 
@@ -93,6 +104,7 @@ func handleDNS(w dns.ResponseWriter, req *dns.Msg, config MadnsConfig) {
 
 	// DETERMINE WHICH CONFIG APPLIES
 	var c MadnsSubConfig
+	var ck string
 	processThis := false
 	for k, v := range config.Handlers {
 		if k == "." { // check default case last
@@ -102,6 +114,7 @@ func handleDNS(w dns.ResponseWriter, req *dns.Msg, config MadnsConfig) {
 		handlerFqdn := strings.ToLower(dns.Fqdn(k))
 
 		if reqFqdn == handlerFqdn || strings.HasSuffix(reqFqdn, "."+handlerFqdn) {
+			ck = k
 			c = v
 			processThis = true
 			break
@@ -110,6 +123,7 @@ func handleDNS(w dns.ResponseWriter, req *dns.Msg, config MadnsConfig) {
 	if !processThis {
 		cnf, ok := config.Handlers["."] // is there a default handler?
 		if ok {
+			ck = "."
 			c = cnf
 			processThis = true
 		}
@@ -125,67 +139,17 @@ func handleDNS(w dns.ResponseWriter, req *dns.Msg, config MadnsConfig) {
 
 	// REDIRECT, if desired (mutually exclusive with RESPOND)
 	if len(c.Redirect) > 0 {
-		dnsClient := &dns.Client{Net: "udp", ReadTimeout: 4 * time.Second, WriteTimeout: 4 * time.Second, SingleInflight: true}
-		if _, ok := w.RemoteAddr().(*net.TCPAddr); ok {
-			dnsClient.Net = "tcp"
-		}
-
-		slog.Info("redirecting using protocol: " + dnsClient.Net)
-
-		retries := 1
-	retry:
-		r, _, err := dnsClient.Exchange(req, c.Redirect)
-		if err == nil {
-			r.Compress = true
-			w.WriteMsg(r)
-		} else {
-			if retries > 0 {
-				retries--
-				slog.Debug("retrying...")
-				goto retry
-			} else {
-				slog.Warn(fmt.Sprintf("failure to forward request %q\n", err))
-				m := new(dns.Msg)
-				m.SetReply(req)
-				m.SetRcode(req, dns.RcodeServerFailure)
-				w.WriteMsg(m)
-			}
-		}
-		// RESPOND, if desired (mutually exclusive with REDIRECT)
+		handleRedirect(w,req,c.Redirect)
+	// RESPOND, if desired (mutually exclusive with REDIRECT)
 	} else if len(c.Respond) > 0 {
-		m := new(dns.Msg)
-		m.SetReply(req)
-		m.SetRcode(req, dns.RcodeSuccess)
-
-		m.Answer = make([]dns.RR, len(req.Question))
-		for i := range req.Question {
-			slog.Info("Responding to " + req.Question[i].Name + " with " + c.Respond)
-
-			ip := net.ParseIP(c.Respond)
-			if ip == nil {
-				// This is not a valid IP address, so assume it's a CNAME
-				rr := new(dns.CNAME)
-				rr.Hdr = dns.RR_Header{Name: req.Question[i].Name,
-					Rrtype: dns.TypeCNAME, Class: dns.ClassINET, Ttl: 0}
-				rr.Target = strings.TrimSuffix(c.Respond, ".") + "."
-				m.Answer[i] = rr
-			} else if ip.To4() == nil {
-				// This is an IPv6 address, so do a AAAA record
-				rr := new(dns.AAAA)
-				rr.Hdr = dns.RR_Header{Name: req.Question[i].Name,
-					Rrtype: dns.TypeAAAA, Class: dns.ClassINET, Ttl: 0}
-				rr.AAAA = ip
-				m.Answer[i] = rr
-			} else {
-				// This is an IPv4 address, so do an A record
-				rr := new(dns.A)
-				rr.Hdr = dns.RR_Header{Name: req.Question[i].Name,
-					Rrtype: dns.TypeA, Class: dns.ClassINET, Ttl: 0}
-				rr.A = ip
-				m.Answer[i] = rr
-			}
-		}
-		w.WriteMsg(m)
+		handleRespond(w,req,c.Respond)
+	} else if c.Rebind != nil && len(c.Rebind.Addrs) > 0 {
+		rc := c.Rebind
+		// Do round robin on the list of addrs (but concurrently-safe)
+		ctrAny, _ := RebindMap.LoadOrStore(ck, &atomic.Uint64{})
+		ctr, _ := ctrAny.(*atomic.Uint64)
+		respond := rc.Addrs[(ctr.Add(1) - 1) % uint64(len(rc.Addrs))] // We want the pre-increment value, hence -1
+		handleRespond(w,req,respond)
 	}
 
 	body := "source: " + w.RemoteAddr().String() + "\n" +
@@ -201,4 +165,69 @@ func handleDNS(w dns.ResponseWriter, req *dns.Msg, config MadnsConfig) {
 	if len(c.NotifySlack) > 0 {
 		sendSlackMessage(c.NotifySlack, body)
 	}
+}
+
+func handleRedirect(w dns.ResponseWriter, req *dns.Msg, redirect string) {
+	dnsClient := &dns.Client{Net: "udp", ReadTimeout: 4 * time.Second, WriteTimeout: 4 * time.Second, SingleInflight: true}
+	if _, ok := w.RemoteAddr().(*net.TCPAddr); ok {
+		dnsClient.Net = "tcp"
+	}
+
+	slog.Info("redirecting using protocol: " + dnsClient.Net)
+
+	retries := 1
+	retry:
+	r, _, err := dnsClient.Exchange(req, redirect)
+	if err == nil {
+		r.Compress = true
+		w.WriteMsg(r)
+	} else {
+		if retries > 0 {
+			retries--
+			slog.Debug("retrying...")
+			goto retry
+		} else {
+			slog.Warn(fmt.Sprintf("failure to forward request %q\n", err))
+			m := new(dns.Msg)
+			m.SetReply(req)
+			m.SetRcode(req, dns.RcodeServerFailure)
+			w.WriteMsg(m)
+		}
+	}
+}
+
+func handleRespond(w dns.ResponseWriter, req *dns.Msg, respond string) {
+	m := new(dns.Msg)
+	m.SetReply(req)
+	m.SetRcode(req, dns.RcodeSuccess)
+
+	m.Answer = make([]dns.RR, len(req.Question))
+	for i := range req.Question {
+		slog.Info("Responding to " + req.Question[i].Name + " with " + respond)
+
+		ip := net.ParseIP(respond)
+		if ip == nil {
+			// This is not a valid IP address, so assume it's a CNAME
+			rr := new(dns.CNAME)
+			rr.Hdr = dns.RR_Header{Name: req.Question[i].Name,
+			Rrtype: dns.TypeCNAME, Class: dns.ClassINET, Ttl: 0}
+			rr.Target = strings.TrimSuffix(respond, ".") + "."
+			m.Answer[i] = rr
+		} else if ip.To4() == nil {
+			// This is an IPv6 address, so do a AAAA record
+			rr := new(dns.AAAA)
+			rr.Hdr = dns.RR_Header{Name: req.Question[i].Name,
+			Rrtype: dns.TypeAAAA, Class: dns.ClassINET, Ttl: 0}
+			rr.AAAA = ip
+			m.Answer[i] = rr
+		} else {
+			// This is an IPv4 address, so do an A record
+			rr := new(dns.A)
+			rr.Hdr = dns.RR_Header{Name: req.Question[i].Name,
+			Rrtype: dns.TypeA, Class: dns.ClassINET, Ttl: 0}
+			rr.A = ip
+			m.Answer[i] = rr
+		}
+	}
+	w.WriteMsg(m)
 }
